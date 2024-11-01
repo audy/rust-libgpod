8a2b56384ee2e0f9c3bc42094f73f71618392f43
Austin Richardson <accounts@agdr.org>
2024-10-31 22:27:00 -0700

    Wrap unsafe code

--- src/main.rs
+++ src/main.rs
@@ -4,48 +4,60 @@ use libc::{c_char, c_void};
 use std::ffi::{CStr, CString};
 use std::ptr;
 
+#[allow(warnings)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-unsafe fn get_cstr_from_ptr(ptr: *const c_char) -> String {
-    if ptr.is_null() {
-        "null".to_string()
-    } else {
-        CStr::from_ptr(ptr).to_string_lossy().into_owned()
-    }
+fn get_cstr_from_ptr(ptr: *const c_char) -> String {
+    let res = unsafe {
+        if ptr.is_null() {
+            "null".to_string()
+        } else {
+            CStr::from_ptr(ptr).to_string_lossy().to_string()
+        }
+    };
+    res
 }
 
-unsafe fn list_ipod_music(ipod_mountpoint: &str) {
+fn fetch_tracks(ipod_mountpoint: &str) -> Vec<String> {
     // Convert Rust string to C string
     let c_mountpoint = CString::new(ipod_mountpoint).expect("CString::new failed");
 
-    // Open the iPod database
-    let db = itdb_parse(c_mountpoint.as_ptr(), ptr::null_mut());
-    if db.is_null() {
-        eprintln!("Failed to open iPod database.");
-        return;
+    let mut tracks: Vec<String> = Vec::new();
+    unsafe {
+        // Open the iPod database
+        let db = itdb_parse(c_mountpoint.as_ptr(), ptr::null_mut());
+
+        // if db.is_null() {
+        //    eprintln!("Failed to open iPod database.");
+        //    return;
+        // }
+
+        let mut track = (*db).tracks;
+
+        while !track.is_null() {
+            let itdb_track = (*track).data as *mut Itdb_Track;
+
+            let title = get_cstr_from_ptr((*itdb_track).title);
+            let artist = get_cstr_from_ptr((*itdb_track).artist);
+
+            let track_str = format!("{} - \"{}\"", artist, title);
+
+            track = (*track).next;
+
+            tracks.push(track_str);
+        }
+
+        // Free the iPod database
+        itdb_free(db);
     }
 
-    let mut track = (*db).tracks;
-
-    while !track.is_null() {
-        let itdb_track = (*track).data as *mut Itdb_Track;
-
-        let title = get_cstr_from_ptr((*itdb_track).title);
-        let artist = get_cstr_from_ptr((*itdb_track).artist);
-
-        println!("{} - \"{}\"", artist, title);
-
-        track = (*track).next;
-    }
-
-    // Free the iPod database
-    itdb_free(db);
+    tracks
 }
 
 fn main() {
     let ipod_mountpoint = "/mnt/f"; // Change this to your iPod's mount point
 
-    unsafe {
-        list_ipod_music(ipod_mountpoint);
-    }
+    let tracks = fetch_tracks(ipod_mountpoint);
+
+    dbg!(tracks);
 }
